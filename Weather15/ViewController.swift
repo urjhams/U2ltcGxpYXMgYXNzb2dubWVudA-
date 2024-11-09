4c608d8ddcd368fe7da1fd33dada77a3adccfe0d
Quân Đinh <17819030+urjhams@users.noreply.github.com>
2024-11-09 08:57:45 +0100

    create initial project and remove storyboard

---
+++ Weather15/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather15
+//
+//  Created by Quân Đinh on 09.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    view.backgroundColor = .cyan
+  }
+
+
+}
+
